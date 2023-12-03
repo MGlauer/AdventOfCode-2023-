cd6bae90f7c269ac67e6ede469ad8ccb587a3e9d
MGlauer <martinglauer89@gmail.com>
2023-12-03 10:28:25 +0100

    Add some comments and refactor

--- martin/day3/solution1.py
+++ martin/day3/solution1.py
@@ -1,33 +1,46 @@
-def is_symbol(cell:str):
-    return not (cell.isdigit() or cell==".")
+def is_symbol(cell: str):
+    return not (cell.isdigit() or cell == ".")
+
 
 def run():
     with open("input.txt") as input_file:
-        node_values = {(x,y):v for (y, l) in enumerate(input_file) for (x, v) in enumerate(l.strip())}  # Iterate over all lines in file and produce the sum of `get_first_and_last_digit(line)`
+        node_values = {
+            (x, y): v
+            for (y, l) in enumerate(input_file)
+            for (x, v) in enumerate(l.strip())
+        }  # Build a map (x,y) -> (symbol at (x,y))
 
     nodes = list(node_values.keys())
-    width = max(x for (x,y) in nodes)+1
-    height = max(y for (x,y) in nodes)+1
+    width = max(x for (x, y) in nodes) + 1
+    height = max(y for (x, y) in nodes) + 1
 
     adjacent_numbers = []
 
-    for y in range(height): # For each row
+    for y in range(height):  # For reach row ..
         x = 0
         concat_number = ""
-        while x < width:
-            cell_value = node_values[(x,y)]
-            if node_values[(x, y)].isdigit():
+        while x < width:  # .. iterate over each character in that row.
+            cell_value = node_values[(x, y)]
+            if node_values[(x, y)].isdigit():  # store consecutive numbers
                 concat_number += cell_value
-            if (not node_values[(x, y)].isdigit() and concat_number) or x == width-1:
-                adjacent_cells = [(x+dx, y+dy) for dx in range(-len(concat_number)-1,1) for dy in range(-1,2) if 0<=x+dx<width and 0<=y+dy<height]
+            # If the number *or the line* ended
+            if (not node_values[(x, y)].isdigit() and concat_number) or x == width - 1:
+                adjacent_cells = [
+                    (x + dx, y + dy)
+                    for dx in range(-len(concat_number) - 1, 1)
+                    for dy in range(-1, 2)
+                    if 0 <= x + dx < width and 0 <= y + dy < height
+                ]  # Collect a cells surrounding the number
 
+                # Check wether surrounding cells contain a symbol
                 if any(is_symbol(node_values[c]) for c in adjacent_cells):
+                    # if so, store the number
                     adjacent_numbers.append(int(concat_number))
+                # delete storesd number
                 concat_number = ""
-
             x += 1
-
     print(sum(adjacent_numbers))
+
 
 if __name__ == "__main__":
     run()
--- martin/day3/solution2.py
+++ martin/day3/solution2.py
@@ -1,33 +1,48 @@
-def is_symbol(cell:str):
-    return not (cell.isdigit() or cell==".")
+def is_symbol(cell: str):
+    return not (cell.isdigit() or cell == ".")
+
 
 def run():
     with open("input.txt") as input_file:
-        node_values = {(x,y):v for (y, l) in enumerate(input_file) for (x, v) in enumerate(l.strip())}  # Iterate over all lines in file and produce the sum of `get_first_and_last_digit(line)`
+        node_values = {
+            (x, y): v
+            for (y, l) in enumerate(input_file)
+            for (x, v) in enumerate(l.strip())
+        }
 
     nodes = list(node_values.keys())
-    width = max(x for (x,y) in nodes)+1
-    height = max(y for (x,y) in nodes)+1
+    width = max(x for (x, y) in nodes) + 1
+    height = max(y for (x, y) in nodes) + 1
 
-    star_neighbors = {}
-    for y in range(height): # For each row
+    # We build a map (star positions (x,y)) -> (numbers around (x,y))asd
+    star_neighbors = dict()
+
+    for y in range(height):
         x = 0
         concat_number = ""
         while x < width:
-            cell_value = node_values[(x,y)]
+            cell_value = node_values[(x, y)]
             if node_values[(x, y)].isdigit():
                 concat_number += cell_value
-            if (not node_values[(x, y)].isdigit() and concat_number) or x == width-1:
-                adjacent_cells = [(x+dx, y+dy) for dx in range(-len(concat_number)-1,1) for dy in range(-1,2) if 0<=x+dx<width and 0<=y+dy<height]
+            if (not node_values[(x, y)].isdigit() and concat_number) or x == width - 1:
+                adjacent_cells = [
+                    (x + dx, y + dy)
+                    for dx in range(-len(concat_number) - 1, 1)
+                    for dy in range(-1, 2)
+                    if 0 <= x + dx < width and 0 <= y + dy < height
+                ]
                 for c in adjacent_cells:
-                    if node_values[c] ==  "*":
+                    # If there is a star, store the number
+                    if node_values[c] == "*":
                         if c in star_neighbors:
                             star_neighbors[c].append(int(concat_number))
                         else:
                             star_neighbors[c] = [int(concat_number)]
                 concat_number = ""
             x += 1
-    print(sum(n[0]*n[1] for _, n in star_neighbors.items() if len(n) ==2))
+
+    # sum of the products of stars that are surrounded by exactly two numbers
+    print(sum(n[0] * n[1] for _, n in star_neighbors.items() if len(n) == 2))
 
 
 if __name__ == "__main__":
